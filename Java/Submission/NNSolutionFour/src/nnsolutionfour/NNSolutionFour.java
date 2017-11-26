539f4b06af1df98b32fc7f16609166fbfa223ae5
Aditya Kurude <akurude@berlinux-solutions.de>
2017-11-26 14:35:06 +0100

    Code cleaning

--- Java/Submission/NNSolutionFour/src/nnsolutionfour/NNSolutionFour.java
+++ Java/Submission/NNSolutionFour/src/nnsolutionfour/NNSolutionFour.java
@@ -20,10 +20,9 @@ import static nnsolutionfour.ArtNeuralNet.showVector;
 public class NNSolutionFour {
 
     public static final boolean debugMode = false;
-    public static final boolean isWeightInput = false;
+    public static final boolean isWeightInput = true;
     public static double eta = 0.0;
 
-    /* or false :-) */
     /**
      * @param args the command line arguments
      */
@@ -118,6 +117,7 @@ public class NNSolutionFour {
         int validationSamples = (totalSamples - trainingSamples);
         for (int epoc = 0; epoc < epocs; epoc++) {
 
+            System.out.println(" training epoc number : "+ epoc);
             for (int n_trSample = 0; n_trSample < trainingSamples; n_trSample++) {
                 myNet.trainNetwork(arr_inputVal.get(n_trSample), arr_targetVal.get(n_trSample));
             }
@@ -133,216 +133,7 @@ public class NNSolutionFour {
 
         System.out.println(" Total errors in prediction = " + myNet.error_count);
         
-        /*try {
-
-                for (int epoc = 1; epoc < epocs; epoc++) {
-
-                    if (epoc % 100 == 0) {
-                        System.out.println(" Epoc number " + epoc + " RMS error " + myNet.getRMSerr());
-
-                    }
-
-                    int sample_num = 0;
-                    br = new BufferedReader(new FileReader(csvFile));
-
-                    // start training
-                    while ((line = br.readLine()) != null) {
-                        sample_num++;
-//                    System.out.println(" *************************** Sample = " + sample_num);
-
-                        //clear all previous inputs and outputs vectors
-                        inputVal.clear();
-                        targetVal.clear();
-                        resultVal.clear();
-
-                        // use comma as separator
-                        String[] sample = line.split(cvsSplitBy);
-                        for (int feature = 0; feature < 57; feature++) {
-//                    System.out.println(" feature " + feature + " is " + sample[feature]);
-                            inputVal.add(Double.parseDouble(sample[feature]));
-                        }
-                        targetVal.add(Double.parseDouble(sample[57]));
-//                System.out.println("");
-
-                        myNet.trainNetwork(inputVal, targetVal, resultVal);
-                    }
-
-                    if (epoc % 100 == 0) {
-                        System.out.println(" Epoc number " + epoc + " RMS error " + myNet.getRMSerr());
-                    System.out.println(" Trained for samples = " + sample_num);
-
-                    }
-                }
-
-                String csvValidateFile = "/home/ubuntu-admin/SharedFolder/EIT_Studies/BME/Study/IntelligentDataAnalysis/validate.csv";
-                BufferedReader brValidate = null;
-                String validateline = "";
-                String validatecvsSplitBy = ",";
-
-                // verify results 
-                int validatesample_num = 0;
-
-                System.out.println(" Total Email samples = " + verifySamples);
-
-                brValidate = new BufferedReader(new FileReader(csvValidateFile));
-
-                // start training
-                while ((validateline = brValidate.readLine()) != null) {
-                    validatesample_num++;
-                    //clear all previous inputs and outputs vectors
-                    inputVal.clear();
-                    targetVal.clear();
-                    resultVal.clear();
-
-                    // use comma as separator
-                    String[] sample = validateline.split(cvsSplitBy);
-                    for (int feature = 0; feature < 57; feature++) {
-//                    System.out.println(" feature " + feature + " is " + sample[feature]);
-                        inputVal.add(Double.parseDouble(sample[feature]));
-                    }
-                    targetVal.add(Double.parseDouble(sample[57]));
-//                System.out.println("");
-
-                    myNet.verifyNetwork(inputVal, targetVal, resultVal);
-                }
-
-                System.out.println(" Total validation samples = " + validatesample_num);
-                System.out.println(" Total Emails mis-classified = " + myNet.error_count);
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (br != null) {
-                    try {
-                        br.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }*/
- /*
-        System.out.println("Enter Teaching parameters of the ArtNeural Network: ");
-        Scanner scanner = new Scanner(System.in);
-        String str_teachingParam = scanner.nextLine();
-
-        if (debugMode) {
-            System.out.println("Your Teaching parameters are " + str_teachingParam);
-        }
-
-        String[] teaching_array = str_teachingParam.split(",", -1);
-        int epocs = Integer.parseInt(teaching_array[0]);
-
-        //assign learning rate
-        eta = Double.parseDouble(teaching_array[1]);
-        Double sample_div_factor = Double.parseDouble(teaching_array[2]);
-
-        System.out.println("Enter architecture of the ArtNeural Network: ");
-        String str_architecture = scanner.nextLine();
-
-        if (debugMode) {
-            System.out.println("Your architecture is " + str_architecture);
-        }
-
-        String[] array = str_architecture.split(",", -1);
-        ArrayList<Integer> architecture = new ArrayList<Integer>();
-
-        for (int num_layers = 0; num_layers < array.length; num_layers++) {
-
-            int num_neurons = Integer.parseInt(array[num_layers]);
-            architecture.add(num_neurons);
-        }
-
-        ArtNeuralNet myNet = new ArtNeuralNet(architecture);
-
-        
-        //accept Input sample values from user
-        System.out.println("Enter number of samples : ");
-        String str_number_samples = scanner.nextLine();
-        System.out.println(" Number of inputs " + str_number_samples);
-
-        int num_samples = Integer.parseInt(str_number_samples);
-
-        ArrayList<Double> inputVal = new ArrayList<Double>();
-        ArrayList<ArrayList<Double>> inputSamples = new ArrayList<ArrayList<Double>>();
-
-        for (int n_sample = 0; n_sample < num_samples; n_sample++) {
-            System.out.println("Enter input values for sample " + n_sample);
-            inputVal.clear();
-
-            String str_inputVal = scanner.nextLine();
-
-            String[] arr_inputVal = str_inputVal.split(",", -1);
-
-            for (int num_val = 0; num_val < arr_inputVal.length - 1; num_val++) {
-
-                Double input_val = Double.parseDouble(arr_inputVal[num_val]);
-                inputVal.add(input_val);
-            }
-
-            inputSamples.add(inputVal);
-        }
-
-        int trainingSamples = (int) (inputSamples.size() * sample_div_factor);
-        int validationSamples = (inputSamples.size() - trainingSamples);
-
-        //Training process
-        for (int n_epoc = 0; n_epoc < epocs; n_epoc++) {
-
-            for (int num_trainingSample = 0; num_trainingSample < trainingSamples; num_trainingSample++) {
-
-                ArrayList<Double> currentSample = inputSamples.get(num_trainingSample);
-
-                //output value is not displayed for this part of the solution
-                //myNet.showVector(outputVal);
-                ArrayList<Double> targetVal = new ArrayList<Double>();
-
-                //take the last value from input vector as target value
-                Double input_val = currentSample.get(currentSample.size() - 1);
-                targetVal.add(input_val);
-
-                //last value is target value hence removed.
-                int num_features = currentSample.size() - 2;
-                myNet.forwardPass(currentSample);
-
-                ArrayList<Double> outputVal = new ArrayList<Double>();
-
-                myNet.getResult(outputVal);
-
-                myNet.backwordPass(targetVal);
-            }
-
-        }
-
-        //Validation Process
-        for (int num_validationSample = 0; num_validationSample < validationSamples; num_validationSample++) {
-
-            ArrayList<Double> currentSample = inputSamples.get(num_validationSample);
-
-            //output value is not displayed for this part of the solution
-            //myNet.showVector(outputVal);
-            ArrayList<Double> targetVal = new ArrayList<Double>();
-
-            //take the last value from input vector as target value
-            Double input_val = currentSample.get(currentSample.size() - 1);
-            targetVal.add(input_val);
-
-            System.out.print(" Expected value : " + input_val);
-            //last value is target value hence removed.
-            int num_features = currentSample.size() - 2;
-            myNet.forwardPass(currentSample);
-
-            ArrayList<Double> outputVal = new ArrayList<Double>();
-
-            myNet.getResult(outputVal);
-            System.out.print(" Output result : ");
-            myNet.showVector(outputVal);
-
-        }
-        
-        
-         */
+       
     }
 
 }
--- Java/Submission/NNSolutionFour/src/nnsolutionfour/NeuronConnect.java
+++ Java/Submission/NNSolutionFour/src/nnsolutionfour/NeuronConnect.java
@@ -14,7 +14,6 @@ import java.util.Random;
 public class NeuronConnect {
 
     public Double weight;
-    public Double deltaWeight;
 
     public NeuronConnect(boolean isBiasNeuron) {
         //If the neuron is bias neuron then initialize its weight to zero.
@@ -29,21 +28,4 @@ public class NeuronConnect {
 
         return num;
     }
-//        public Double deltaWeight;
-//public Double weight;
-//
-//    public NeuronConnect(boolean isBiasNeuron) {
-//        //If the neuron is bias neuron then initialize its weight to zero.
-//        weight = isBiasNeuron ? 0.0 : randomWeight();
-//    }
-//
-//    public Double randomWeight() {
-//        Random rand = new Random();
-//        double standardDeviation = 0.1;
-//        double requiredMean = 0;
-//        Double num = rand.nextGaussian() * standardDeviation + requiredMean;
-//
-//        return num;
-//    }
-
 }
